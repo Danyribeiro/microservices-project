3a2fea62f4b3a1f10f8643955211218555070601
Danielly <dany.s.ribeiro@gmail.com>
2021-08-26 18:40:28 -0300

    First commit

---
+++ product-catalog/src/main/java/com/one/digital/innovation/experts/product/catalog/config/ElasticsearchEntityMapper.java
@@ -0,0 +1,8 @@
+package com.one.digital.innovation.experts.product.catalog.config;
+
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchCustomConversions;
+
+public class ElasticsearchEntityMapper {
+    public void setConversions(ElasticsearchCustomConversions elasticsearchCustomConversions) {
+    }
+}
