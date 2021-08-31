2f2e8487be1db20f4c1d8313921b18c42164218c
Danielly <dany.s.ribeiro@gmail.com>
2021-08-31 18:30:24 -0300

    finished project

--- product-catalog/src/main/java/com/one/digital/innovation/experts/product/catalog/repository/ProductRepository.java
+++ product-catalog/src/main/java/com/one/digital/innovation/experts/product/catalog/repository/ProductRepository.java
@@ -1,2 +1,8 @@
-package com.one.digital.innovation.experts.product.catalog.repository;public interface ProductRepository {
+package com.one.digital.innovation.experts.product.catalog.repository;
+
+import com.one.digital.innovation.experts.product.catalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+    Iterable<Product> findByName(String name);
 }
